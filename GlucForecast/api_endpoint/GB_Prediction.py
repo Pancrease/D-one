0c80d757e941a1d0e8eb4e25f9750562289c53ed
oussama95boussaid <oussama.bouss3id95@gmail.com>
2023-12-31 04:13:06 +0100

    feat: Insulin API Done!

--- GlucForecast/api_endpoint/GB_Prediction.py
+++ GlucForecast/api_endpoint/GB_Prediction.py
@@ -1,22 +1,55 @@
 import uvicorn
 import pickle as pck
 from fastapi import FastAPI
-from data_strut import GlucoseData
+from data_strut import GlucoseData,GlucForecast,InsuDose
+from keras.models import load_model
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+
 
 app=FastAPI()
-model_path="../saved_version.pkl"
 
+# GlucoseData
+BG_model_path="../BG_metadata.pkl"
 def read_model(path:str):
     with open(path,"rb") as model:
         return pck.load(model)
+BG_model=read_model(BG_model_path)
 
-model=read_model(model_path)
-    
-@app.post("/predictions") 
+@app.post("/predictions/BG") 
 def predictions(data:GlucoseData):
     features=[[data.CGM,data.CHO,data.insuline]]
-    prediction=model.predict(features).tolist()
+    prediction=BG_model.predict(features).tolist()
     return {"dd":prediction}
+
+
+# GlucForecast
+Forcast_model_path="../BG_metadata.pkl"
+Forcast_model=load_model(Forcast_model_path)
+
+# InsuDose
+Insulin_model_path="../Insulin_metadata.pkl"
+def read_model(path:str):
+    with open(path,"rb") as model:
+        return pck.load(model)
+Insulin_model=read_model(Insulin_model_path)
+@app.post("/predictions/Insulin") 
+def InsuDose_Pipline(data:InsuDose):
+    features = np.array(data.BG).reshape(-1,1)
+     # Feature scaling
+    scaler = MinMaxScaler(feature_range = (0,1))
+    Glucose_act = scaler.fit_transform(features)
+    
+    Insulin_pred = Insulin_model.predict(Glucose_act)
+
+    Insulin_pred = np.array(Insulin_pred).reshape(-1,1)
+    #Inverse transform predictions
+    BG_pred = scaler.inverse_transform(Insulin_pred)
+    return BG_pred.reshape(1)[0]
+    
+   
+
 
 if __name__ == "main":
     uvicorn.run(app,host="127.0.0.1",port=8000)
--- GlucForecast/api_endpoint/data_strut.py
+++ GlucForecast/api_endpoint/data_strut.py
@@ -4,3 +4,13 @@ class GlucoseData(BaseModel):
     CGM:float
     CHO:float
     insuline:float
+    
+
+    
+class GlucForecast(BaseModel):
+    BG:[]                     # List of 4 values of Blood Glucose  
+
+    
+    
+class InsuDose(BaseModel):
+    BG:float                   # Predicted Blood Glucose for 3h in the future
